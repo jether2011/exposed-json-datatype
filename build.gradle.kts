c325679ba0da1f23a486bba6f0c41745382e3b31
Jether Rodrigues do Nascimento <jetherrodrigues@gmail.com>
2019-09-05 16:09:28 -0300

    updated build gradle file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,2 +1,22 @@
-group = "com.jetherrodrigues"
-version = "1.0-SNAPSHOT"
+plugins {
+    id("org.jetbrains.kotlin.jvm").version("1.3.10")
+    application
+}
+
+repositories {
+    jcenter()
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    compile ("org.jetbrains.exposed:exposed:0.16.1")
+    compile ("io.azam.ulidj:ulidj:1.0.0")
+    compile ("org.postgresql:postgresql:42.2.2")
+
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+    testImplementation("org.slf4j", "slf4j-log4j12", "1.7.26")
+    testImplementation("log4j", "log4j", "1.2.17")
+}
