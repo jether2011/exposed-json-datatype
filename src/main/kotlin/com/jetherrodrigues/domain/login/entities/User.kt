4c1fec699d401973759341cd7976918689ac0bb9
Jether Rodrigues do Nascimento <jetherrodrigues@gmail.com>
2019-09-07 16:20:14 -0300

    added user class

--- src/main/kotlin/com/jetherrodrigues/domain/login/entities/User.kt
+++ src/main/kotlin/com/jetherrodrigues/domain/login/entities/User.kt
@@ -2,9 +2,9 @@ package com.jetherrodrigues.domain.login.entities
 
 import com.jetherrodrigues.commons.ULID
 
-data class User(
-        val id: ULID = ULID.random(),
-        val name: String,
-        val email: String,
-        val roles: MutableList<Role> = mutableListOf()
+data class User (
+    val id: ULID = ULID.random(),
+    val name: String,
+    val email: String,
+    val roles: List<*>
 )
